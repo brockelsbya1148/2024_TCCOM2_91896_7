f38bbd714de60c23a5739a7eba603ade54620ec7
brockelsbya1148 <110875387+brockelsbya1148@users.noreply.github.com>
2024-06-11 10:34:40 +1200

    integrated ingredients into base code

--- B_00_assessment.py
+++ B_00_assessment.py
@@ -30,34 +30,85 @@ def not_blank(question):
             return response
 
 
-# Checks that answer to a question is either yes or no
-def yes_no(question):
+# checks that users enter a valid response (e.g. yes/ no
+# units) based on a list of options
+def string_checker(question, valid_responses):
+    error = "Please choose a valid answer"
 
     while True:
+
         response = input(question).lower()
 
-        # If response is yes/no or y/n, continue
-        if response == "yes" or response == "y":
-            return "yes"
+        for item in valid_responses:
+            if response == item:
+                return item
 
-        elif response == "no" or response == "n":
-            return "no"
-
-        else:
-            print("Please enter Yes or No\n")
-            continue
+        print(error)
 
 
 # Main Routine Starts Here
 
+# String checker lists
+yes_no = {
+    "yes": "yes", "y": "yes",
+    "no": "no", "n": "no"
+}
+
+unit_dict = {
+    "kilograms": "kg", "kg": "kg",
+    "grams": "g", "g": "g",
+    "milligrams": "mg", "mg": "mg",
+    "litres": "l", "l": "l",
+    "millilitres": "ml", "ml": "ml",
+    "cups": "c", "c": "c",
+    "tablespoons": "tbsp", "tbsp": "tbsp",
+    "teaspoons": "tsp", "tsp": "tsp",
+    "none": ""
+}
+
 # Ask users if they want instructions or not
-instruction_yn = yes_no("Do you want to see the instructions ")
+instruction_yn = string_checker("Do you want to see the instructions ", yes_no.keys())
+# y/n conversion
+instruction_yn = yes_no[instruction_yn]
+# print instructions
 if instruction_yn == "yes":
     print("\nInstructions go here")
 
-# Find name of recipe and hiw many servings
+# Find name of recipe and how many servings
 recipe_name = not_blank("\nWhat is the name of your recipe? ")
 recipe_servings = num_check("How many servings does it make? ", "Please enter a valid amount of servings", int)
 
+# *** Ask user for ingredients ***
+
+# List to store ingredients
+ingredients = []
+
+while True:
+    ing_name = not_blank("\nWhat is the name of your ingredient? (xxx if entered all ingredients) ")
+    if ing_name.lower() == "xxx":
+        break
+    ing_unit = string_checker("What unit of measurement does the ingredient use? (or none if no units, e.g. eggs) ",
+                              unit_dict.keys())
+    ing_amount = num_check("How many of this unit? ", "Please enter a number more than 0 (no fractions)", float)
+
+    # Unit conversion
+    ing_unit = unit_dict[ing_unit]
+
+    # Add ingredient to the list
+    ingredients.append((ing_name, ing_amount, ing_unit))
+
+# *** Print area ***
+
+# Print name and servings
 print(f"\n**** {recipe_name} ****\n")
 print(f"Servings: {recipe_servings}")
+
+
+# Print all ingredients
+print("\n*** Ingredients ***\n")
+for ing_name, ing_amount, ing_unit in ingredients:
+    # if it's a whole number, make it an int
+    if ing_amount == int(ing_amount):
+        ing_amount = int(ing_amount)
+    # print list
+    print(f"{ing_name}: {ing_amount} {ing_unit}")
